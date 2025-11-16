967780c84361d3aa45d56a423f9f00b54ba15140
Nine12 <banianjay004@gmail.com>
2025-11-16 18:09:41 +0700

    Delete public directory

--- public/ninerctaskflow/js/stats-component.js
+++
@@ -1,9 +0,0 @@
-// Stats Card Component
-function StatsCard({ number, label }) {
-  return (
-    <div className="stats-card">
-      <div className="stats-number">{number}</div>
-      <div className="stats-label">{label}</div>
-    </div>
-  );
-}
